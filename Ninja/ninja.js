4a6963aa7311f3ddabab4fcc9041980674198071
Vicente <viperalta@gmail.com>
2021-04-06 14:29:16 -0400

    Ejercicio Ninja agregado

---
+++ Ninja/ninja.js
@@ -0,0 +1,33 @@
+class Ninja{
+    constructor(nombre){
+        this.nombre=nombre;
+        this.salud=100;
+        this.velocidad=3;
+        this.fuerza=3;
+    }
+
+    sayName(){
+        console.log('Hola me llamo '+this.nombre);
+    }
+
+    showStats(){
+        const stats={
+            nombre: this.nombre,
+            fuerza: this.fuerza,
+            velocidad: this.velocidad,
+            salud: this.salud
+        }
+        console.log(stats);
+    }
+
+    drinkSake(){
+        this.salud+=10;
+        console.log(this.nombre+' ha tomado sake. Su salud es de: '+this.salud);
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+ninja1.showStats();
